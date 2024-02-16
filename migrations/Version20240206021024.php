6b5cd8d61a385974ec5530a4070450e9a2e4606d
--repplace-all <rostikkarpenko2607@gmail.com>
2024-02-16 19:41:20 +0200

    ch

--- migrations/Version20240206021024.php
+++ migrations/Version20240206021024.php
@@ -21,13 +21,11 @@ final class Version20240206021024 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE cuisine (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, recipe_id INT NOT NULL, src LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:simple_array)\', INDEX IDX_C53D045F59D8A214 (recipe_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE newsletter_client (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, subscribe_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE recipe (id INT AUTO_INCREMENT NOT NULL, cuisine_id INT DEFAULT NULL, user_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(1024) DEFAULT NULL, image VARCHAR(1024) NOT NULL, publication_date DATE DEFAULT NULL COMMENT \'(DC2Type:date_immutable)\', slug VARCHAR(255) NOT NULL, ingredients LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\', steps LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\', images LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:simple_array)\', INDEX IDX_DA88B137ED4BAC14 (cuisine_id), INDEX IDX_DA88B137A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE refreshToken (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, refresh_token VARCHAR(128) NOT NULL, username VARCHAR(255) NOT NULL, valid DATETIME NOT NULL, crated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_6973EC66C74F2195 (refresh_token), INDEX IDX_6973EC66A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, recipe_id INT DEFAULT NULL, rating INT NOT NULL, text VARCHAR(255) NOT NULL, review_public_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_794381C6A76ED395 (user_id), INDEX IDX_794381C659D8A214 (recipe_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, country VARCHAR(255) DEFAULT NULL, about_me VARCHAR(255) DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045F59D8A214 FOREIGN KEY (recipe_id) REFERENCES recipe (id)');
         $this->addSql('ALTER TABLE recipe ADD CONSTRAINT FK_DA88B137ED4BAC14 FOREIGN KEY (cuisine_id) REFERENCES cuisine (id)');
         $this->addSql('ALTER TABLE recipe ADD CONSTRAINT FK_DA88B137A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE refreshToken ADD CONSTRAINT FK_6973EC66A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
@@ -38,14 +36,12 @@ final class Version20240206021024 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE image DROP FOREIGN KEY FK_C53D045F59D8A214');
         $this->addSql('ALTER TABLE recipe DROP FOREIGN KEY FK_DA88B137ED4BAC14');
         $this->addSql('ALTER TABLE recipe DROP FOREIGN KEY FK_DA88B137A76ED395');
         $this->addSql('ALTER TABLE refreshToken DROP FOREIGN KEY FK_6973EC66A76ED395');
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6A76ED395');
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C659D8A214');
         $this->addSql('DROP TABLE cuisine');
-        $this->addSql('DROP TABLE image');
         $this->addSql('DROP TABLE newsletter_client');
         $this->addSql('DROP TABLE recipe');
         $this->addSql('DROP TABLE refreshToken');
